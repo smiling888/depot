41b1261246072f1659c78ef2600361ebb7fc0569
liaozhenhua <314773862@qq.com>
2012-03-02 21:00:35 +0800

    20120302ch10

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Depot::Application.routes.draw do
+  resources :line_items
+
+  resources :carts
+
+ # get "store/index"
+  #get 'store/index'=>'store#index'
+
   resources :products
+  resources :store
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -51,7 +59,7 @@ Depot::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => 'welcome#index'
-
+ root :to=> 'store#index', :as=>'store'
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
--- test/unit/product_test.rb
+++ test/unit/product_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+test "product is not valid without a unique title - i18n" do
+product = Product.new(:title       => products(:ruby).title,
+					  :description => "yyy" ,
+					  :price 	   => 1,
+					  :image_url     => "fred.gif" )
+
+assert !product.save
+assert_equal I18n.translate('activerecord.errors.messages.taken' ),
+product.errors[:title].join('; ')
+
 end
